54ea77d9c1a0a3deabb159c9198e17156b1a6f40
Олександр Маркович <markoleks08@gmail.com>
2024-11-11 21:07:15 +0200

    */*/

--- Kortex store/SCRIPT/script.js
+++ Kortex store/SCRIPT/script.js
@@ -33,6 +33,8 @@ carousel.addEventListener('mouseleave', () => {
 
     //SALE PRODUCTS
 // Зупинка анімації при наведенні на карусель
+
+/*
 const salecarousel = document.querySelector('.carousel');
 const saleproducts = document.querySelectorAll('.product');
 const nextButton = document.querySelector('.next');
@@ -90,5 +92,5 @@ products.forEach(product => {
         product.style.transform = 'scale(1)';
     });
 });
-
+*/
 
