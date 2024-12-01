9bab7675fbe42d608a742030ef98190cd1dca846
Олександр Маркович <markoleks08@gmail.com>
2024-12-01 18:14:53 +0200

    LOTS

--- Kortex store/SCRIPT/Header.js
+++ Kortex store/SCRIPT/Header.js
@@ -14,3 +14,23 @@ burgerMenu.classList.toggle('active');
         BlackLine.classList.toggle('active');
         SecondBlackLine.classList.toggle('active');
 });
+
+
+// SCROLL HEADER
+
+let lastScrollY = 0;
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+  const currentScrollY = window.scrollY;
+
+  if (currentScrollY > lastScrollY) {
+    // Скрол вниз — ховаємо хедер
+    header.classList.add('hidden');
+  } else {
+    // Скрол вверх — показуємо хедер
+    header.classList.remove('hidden');
+  }
+
+  lastScrollY = currentScrollY;
+});
